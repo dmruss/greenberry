54c45e27eee5ecb2cb7784e9ad027cad7d239ca7
David Russell <45084308+dmruss@users.noreply.github.com>
2023-05-12 17:47:04 +0000

    init parser test

---
+++ tests/parser_test.py
@@ -0,0 +1,17 @@
+#
+# parser tests
+#
+
+import unittest
+import greenberry.utils.parse as parser
+
+
+class GBParserTests(unittest.TestCase):
+    def test_simple_parse(self):
+        g_vars = {'words': ''}
+        words = ['words', 'to', 'test']
+
+
+
+if __name__ == "__main__":
+    unittest.main(exit=False)
