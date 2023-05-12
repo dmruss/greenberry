5e902f048b4f94da4b82353c8c9fbe7b2429dcbe
David Russell <45084308+dmruss@users.noreply.github.com>
2023-05-12 18:30:01 +0000

    add set up class

--- tests/parser_test.py
+++ tests/parser_test.py
@@ -7,14 +7,31 @@ import greenberry.utils.parse as parse
 
 
 class GBParserTests(unittest.TestCase):
-    def test_simple_parse(self):
-        g_vars = {'words': 'a'}
-        words = ['print', 'a b c', 'b', 'c']
-        line = None
-        parser = parse.GreenBerryParse()
-        parser.simple_parse(words, line)
+    @classmethod
+    def setUpClass(self) -> None:
+        self.parser = parse.GreenBerryParse()
+        return
+    
+    def test_simple_parse_print_one_word(self):
+        words = ['print', 'a']
+        self.parser.simple_parse(words=words, line=None)
 
+    def test_simple_parse_print_eval(self):
+        words = ['print', 'eval', '(2 + 3 - 4)']
+        self.parser.simple_parse(words=words, line=None)
 
+    def test_simple_parse_string(self):
+        words = ['print', 'string', 'a', 'cat', 'walks', 'along', 'the', 'path']
+        self.parser.simple_parse(words=words, line=None)
+
+    def test_simple_parse_var_ref(self):
+        # g_vars = {'words': ['x']}
+        # words = ['var', 'x', '=', '1']
+        # self.parser.simple_parse(g_vars, words)
+       # TODO
+
+    
+        
 
 if __name__ == "__main__":
     unittest.main(exit=False)
